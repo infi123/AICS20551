3c0c207104158532e7c4f7b6d4591bda14671ed8
infi123 <133763557+infi123@users.noreply.github.com>
2023-11-21 16:21:40 +0200

    all the algorithms are in the algorithms.py file and working. the gbfs is not working properly, it is not finding the optimal solution.

--- MMN11/algorithms.py
+++ MMN11/algorithms.py
@@ -1,92 +1,418 @@
+import argparse
 from collections import deque
+import timeit
 
-def bfs(matrix):
-    # Define the goal state
-    goal = [[0, 1, 2, 3], [4, 5, 6, 7], [8, 9, 10, 11], [12, 13, 14, 15]]
 
-    # Define the start state
-    start = matrix
+class PuzzleState:
+    def __init__(self, state, parent, move, depth, cost, key):
+        self.state = state
+        self.parent = parent
+        self.move = move
+        self.depth = depth
+        self.cost = cost
+        self.key = key
+        if self.state:
+            self.map = ''.join(str(e) for e in self.state)
+    def __eq__(self, other):
+        return self.map == other.map
+    def __lt__(self, other):
+        return self.map < other.map
+    def __str__(self):
+        return str(self.map)    
 
-    # Create a queue for BFS and enqueue the start state
-    queue = deque([(start, [])])
+#Global variables***********************************************
+GoalState = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+GoalNode = None # at finding solution
+NodesExpanded = 0 #total nodes visited
+MaxSearchDeep = 0 #max deep
+MaxFrontier = 0 #max frontier
 
-    # Create a set to keep track of the explored nodes
-    explored = set()
+def bfs(startState):
 
-    while queue:
-        # Dequeue a node from the queue
-        state, path = queue.popleft()
+    global MaxFrontier, GoalNode, MaxSearchDeep
 
-        # Check if the current state is the goal state
-        if state == goal:
-            return {
-                "name": "BFS",
-                "nodes_opened": len(explored),
-                "path": path,
-            }
+    boardVisited= set()
+    Queue = deque([PuzzleState(startState, None, None, 0, 0, 0)])
 
-        # Mark the node as explored
-        explored.add(str(state))
+    while Queue:
+        node = Queue.popleft()
+        boardVisited.add(node.map)
+        if node.state == GoalState:
+            GoalNode = node
+            return Queue
+        posiblePaths = subNodes(node)
+        for path in posiblePaths:
+            if path.map not in boardVisited:
+                Queue.append(path)
+                boardVisited.add(path.map)
+                if path.depth > MaxSearchDeep:
+                    MaxSearchDeep = MaxSearchDeep + 1
+        if len(Queue) > MaxFrontier:
+            QueueSize = len(Queue)
+            MaxFrontier = QueueSize
 
-        # Get all possible next states
-        next_states = get_next_states(state)
+def subNodes(node):
 
-        for next_state, move in next_states:
-            # If the node has not been explored yet, enqueue it
-            if str(next_state) not in explored:
-                queue.append((next_state, path + [move]))
+    global NodesExpanded
+    NodesExpanded = NodesExpanded+1
 
-    # If there's no solution, return None
+    nextPaths = []
+    nextPaths.append(PuzzleState(move(node.state, 1), node, 1, node.depth + 1, node.cost + 1, 0))
+    nextPaths.append(PuzzleState(move(node.state, 2), node, 2, node.depth + 1, node.cost + 1, 0))
+    nextPaths.append(PuzzleState(move(node.state, 3), node, 3, node.depth + 1, node.cost + 1, 0))
+    nextPaths.append(PuzzleState(move(node.state, 4), node, 4, node.depth + 1, node.cost + 1, 0))
+    nodes=[]
+    for procPaths in nextPaths:
+        if(procPaths.state!=None):
+            nodes.append(procPaths)
+    return nodes
+
+def move(state, direction):
+    #generate a copy
+    newState = state[:]
+    
+    #obtain poss of 0
+    index = newState.index(0)
+
+    if(index==0):
+        if(direction==1):
+            return None
+        if(direction==2):
+            temp=newState[0]
+            newState[0]=newState[3]
+            newState[3]=temp
+        if(direction==3):
+            return None
+        if(direction==4):
+            temp=newState[0]
+            newState[0]=newState[1]
+            newState[1]=temp
+        return newState      
+    if(index==1):
+        if(direction==1):
+            return None
+        if(direction==2):
+            temp=newState[1]
+            newState[1]=newState[4]
+            newState[4]=temp
+        if(direction==3):
+            temp=newState[1]
+            newState[1]=newState[0]
+            newState[0]=temp
+        if(direction==4):
+            temp=newState[1]
+            newState[1]=newState[2]
+            newState[2]=temp
+        return newState    
+    if(index==2):
+        if(direction==1):
+            return None
+        if(direction==2):
+            temp=newState[2]
+            newState[2]=newState[5]
+            newState[5]=temp
+        if(direction==3):
+            temp=newState[2]
+            newState[2]=newState[1]
+            newState[1]=temp
+        if(direction==4):
+            return None
+        return newState
+    if(index==3):
+        if(direction==1):
+            temp=newState[3]
+            newState[3]=newState[0]
+            newState[0]=temp
+        if(direction==2):
+            temp=newState[3]
+            newState[3]=newState[6]
+            newState[6]=temp
+        if(direction==3):
+            return None
+        if(direction==4):
+            temp=newState[3]
+            newState[3]=newState[4]
+            newState[4]=temp
+        return newState
+    if(index==4):
+        if(direction==1):
+            temp=newState[4]
+            newState[4]=newState[1]
+            newState[1]=temp
+        if(direction==2):
+            temp=newState[4]
+            newState[4]=newState[7]
+            newState[7]=temp
+        if(direction==3):
+            temp=newState[4]
+            newState[4]=newState[3]
+            newState[3]=temp
+        if(direction==4):
+            temp=newState[4]
+            newState[4]=newState[5]
+            newState[5]=temp
+        return newState
+    if(index==5):
+        if(direction==1):
+            temp=newState[5]
+            newState[5]=newState[2]
+            newState[2]=temp
+        if(direction==2):
+            temp=newState[5]
+            newState[5]=newState[8]
+            newState[8]=temp
+        if(direction==3):
+            temp=newState[5]
+            newState[5]=newState[4]
+            newState[4]=temp
+        if(direction==4):
+            return None
+        return newState
+    if(index==6):
+        if(direction==1):
+            temp=newState[6]
+            newState[6]=newState[3]
+            newState[3]=temp
+        if(direction==2):
+            return None
+        if(direction==3):
+            return None
+        if(direction==4):
+            temp=newState[6]
+            newState[6]=newState[7]
+            newState[7]=temp
+        return newState
+    if(index==7):
+        if(direction==1):
+            temp=newState[7]
+            newState[7]=newState[4]
+            newState[4]=temp
+        if(direction==2):
+            return None
+        if(direction==3):
+            temp=newState[7]
+            newState[7]=newState[6]
+            newState[6]=temp
+        if(direction==4):
+            temp=newState[7]
+            newState[7]=newState[8]
+            newState[8]=temp
+        return newState
+    if(index==8):
+        if(direction==1):
+            temp=newState[8]
+            newState[8]=newState[5]
+            newState[5]=temp
+        if(direction==2):
+            return None
+        if(direction==3):
+            temp=newState[8]
+            newState[8]=newState[7]
+            newState[7]=temp
+        if(direction==4):
+            return None
+        return newState
+
+def iddfs(startState):
+    global MaxFrontier, GoalNode, MaxSearchDeep
+
+    depth = 0
+    while True:
+        visited = set()
+        root = PuzzleState(startState, None, None, 0, 0, 0)
+        result = dls(root, depth, visited)
+        if result is not None:
+            GoalNode = result
+            return visited
+        depth += 1
+        MaxSearchDeep = max(MaxSearchDeep, depth)
+        MaxFrontier = max(MaxFrontier, len(visited))
+
+def dls(node, depth, visited):
+    if node.map in visited or depth < 0:
+        return None
+    visited.add(node.map)
+    if node.state == GoalState:
+        return node
+    elif depth == 0:
+        return None
+    else:
+        for child in subNodes(node):
+            result = dls(child, depth - 1, visited)
+            if result is not None:
+                return result
     return None
 
-def iddfs(matrix):
-    # Implement Iterative Deepening DFS algorithm
-    # Return the result as a dictionary
-    return {
-        "name": "IDDFS",
-        "nodes_opened": 0,  # Replace with actual value
-        "path": [],  # Replace with actual value
-    }
 
-def gbfs(matrix):
-    # Implement Greedy Best First Search algorithm
-    # Return the result as a dictionary
-    return {
-        "name": "GBFS",
-        "nodes_opened": 0,  # Replace with actual value
-        "path": [],  # Replace with actual value
-    }
+def gbfs(startState):
+    
+    global MaxFrontier, MaxSearchDeep, GoalNode
+    
+    #transform initial state to calculate Heuritic
+    node1 = ""
+    for poss in startState:
+        node1 = node1 + str(poss)
 
-def a_star(matrix):
-    # Implement A* algorithm
-    # Return the result as a dictionary
-    return {
-        "name": "A*",
-        "nodes_opened": 0,  # Replace with actual value
-        "path": [],  # Replace with actual value
-    }
+    #calculate Heuristic and set initial node
+    key = Heuristic(node1)
+    boardVisited= set()
+    Queue = []
+    Queue.append(PuzzleState(startState, None, None, 0, 0, key)) 
+    boardVisited.add(node1)
+    
+    while Queue:
+        Queue.sort(key=lambda o: o.key) 
+        node = Queue.pop(0)
+        if node.state == GoalState:
+            GoalNode = node
+            return GoalNode
+        posiblePaths = subNodes(node)
+        for path in posiblePaths:      
+            thisPath = path.map[:]
+            if thisPath not in boardVisited:
+                key = Heuristic(path.map)
+                path.key = key  # Only use the heuristic value for GBFS
+                Queue.append(path)               
+                boardVisited.add(path.map[:])
+                if path.depth > MaxSearchDeep:
+                    MaxSearchDeep = 1 + MaxSearchDeep
+
+def ast(startState):
+    
+    global MaxFrontier, MaxSearchDeep, GoalNode
+    
+    #transform initial state to calculate Heuritic
+    node1 = ""
+    for poss in startState:
+        node1 = node1 + str(poss)
+
+    #calculate Heuristic and set initial node
+    key = Heuristic(node1)
+    boardVisited= set()
+    Queue = []
+    Queue.append(PuzzleState(startState, None, None, 0, 0, key)) 
+    boardVisited.add(node1)
+    
+    while Queue:
+        Queue.sort(key=lambda o: o.key) 
+        node = Queue.pop(0)
+        if node.state == GoalState:
+            GoalNode = node
+            return Queue
+        posiblePaths = subNodes(node)
+        for path in posiblePaths:      
+            thisPath = path.map[:]
+            if thisPath not in boardVisited:
+                key = Heuristic(path.map)
+                path.key = key + path.depth
+                Queue.append(path)               
+                boardVisited.add(path.map[:])
+                if path.depth > MaxSearchDeep:
+                    MaxSearchDeep = 1 + MaxSearchDeep
+        
+        
 
 
+def Heuristic(node):
+    goal = GoalState
+    count = 0
+    for i in range(len(node)):
+        if int(node[i]) != goal[i]:
+            # Calculate the correct row and column for the current tile
+            correct_row = int(goal.index(int(node[i])) / 3)
+            correct_col = goal.index(int(node[i])) % 3
+            # Calculate the current row and column for the current tile
+            current_row = int(i / 3)
+            current_col = i % 3
+            # If the current tile is not in the correct row or column, increment the count
+            if correct_row != current_row or correct_col != current_col:
+                count += 1
+    return count
 
 
-def get_next_states(matrix):
-    # Find the position of the 0 tile
-    for i in range(4):
-        for j in range(4):
-            if matrix[i][j] == 0:
-                x, y = i, j
+def main():
 
-    # Define the possible moves
-    moves = [(x-1, y, 'U'), (x+1, y, 'D'), (x, y-1, 'L'), (x, y+1, 'R')]
+    global GoalNode
 
-    # Generate the next states
-    next_states = []
-    for x2, y2, move in moves:
-        if 0 <= x2 < 4 and 0 <= y2 < 4:
-            # Copy the current state to create a new state
-            next_state = [row.copy() for row in matrix]
-            # Swap the 0 tile with the adjacent tile
-            next_state[x][y], next_state[x2][y2] = next_state[x2][y2], next_state[x][y]
-            next_states.append((next_state, move))
+    #a = [1,8,2,3,4,5,6,7,0]
+    #point=Heuristic(a)
+    #print(point)
+    #return
+    
+    #info = "6,1,8,4,0,2,7,3,5" #20
+    #info = "8,6,4,2,1,3,5,7,0" #26
+    
+    #Obtain information from calling parameters
+    parser = argparse.ArgumentParser()
+    parser.add_argument('method')
+    parser.add_argument('initialBoard')
+    args = parser.parse_args()
+    data = args.initialBoard.split(",")
 
-    return next_states
+    #Build initial board state
+    InitialState = []
+    InitialState.append(int(data[0]))
+    InitialState.append(int(data[1]))
+    InitialState.append(int(data[2]))
+    InitialState.append(int(data[3]))
+    InitialState.append(int(data[4]))
+    InitialState.append(int(data[5]))
+    InitialState.append(int(data[6]))
+    InitialState.append(int(data[7]))
+    InitialState.append(int(data[8]))
+
+    #Start operation
+    start = timeit.default_timer()
+
+    function = args.method
+    if(function=="bfs"):
+        bfs(InitialState)
+    if(function=="iddfs"):
+        iddfs(InitialState) 
+    if(function=="gbfs"):
+        gbfs(InitialState) 
+    if(function=="ast"):
+        ast(InitialState) 
+
+    stop = timeit.default_timer()
+    time = stop-start
+
+    #Save total path result
+    deep=GoalNode.depth
+    moves = []
+    while InitialState != GoalNode.state:
+        if GoalNode.move == 1:
+            path = 'Up'
+        if GoalNode.move == 2:
+            path = 'Down'
+        if GoalNode.move == 3:
+            path = 'Left'
+        if GoalNode.move == 4:
+            path = 'Right'
+        moves.insert(0, path)
+        GoalNode = GoalNode.parent
+
+    #'''
+    #Print results
+    print("path: ",moves)
+    print("cost: ",len(moves))
+    print("nodes expanded: ",str(NodesExpanded))
+    print("search_depth: ",str(deep))
+    print("MaxSearchDeep: ",str(MaxSearchDeep))
+    print("running_time: ",format(time, '.8f'))
+    #'''
+
+    #Generate output document for grade system
+    #'''
+    file = open('output.txt', 'w')
+    file.write("path_to_goal: " + str(moves) + "\n")
+    file.write("cost_of_path: " + str(len(moves)) + "\n")
+    file.write("nodes_expanded: " + str(NodesExpanded) + "\n")
+    file.write("search_depth: " + str(deep) + "\n")
+    file.write("max_search_depth: " + str(MaxSearchDeep) + "\n")
+    file.write("running_time: " + format(time, '.8f') + "\n")
+    file.close()
+    #'''
+
+if __name__ == '__main__':
+    main()
