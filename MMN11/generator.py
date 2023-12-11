6676fe47be3b523685dfa547ade8d1cf2280fc29
infi123 <roei141105@gmail.com>
2023-12-11 15:30:16 +0200

    d

---
+++ MMN11/generator.py
@@ -0,0 +1,25 @@
+import random
+from algorithms import *
+
+def random_state(steps):
+    # Define the goal state and the possible moves
+    state = [1, 2, 3, 4, 5, 6, 7, 8, 0]
+    moves = [1, 2, 3, 4]
+
+    # Make a number of random moves
+    for _ in range(steps):
+        # Choose a random move and try to apply it
+        while True:
+            chosen_move = random.choice(moves)
+            index = state.index(0)
+            new_state = move(state, index, 3, chosen_move)
+            if new_state is not None:
+                state = new_state
+                break
+
+    return state
+
+# Generate a random state that is 6 to 10 steps away from the goal state
+steps = random.randint(6, 10)
+state = random_state(50)
+print(state)
